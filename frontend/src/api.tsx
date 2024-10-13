2f1f78325f1fb5a7c2ab2c447c05c6820b7fa480
rodionhatskevich <rodionhatskevich@gmail.com>
2024-10-13 22:39:00 +0200

    create search and portfolio

---
+++ frontend/src/api.tsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { ICompanySearch } from "./company";
+
+export interface ISearchResponse {
+  data: ICompanySearch[];
+}
+
+export const searchCompanies = async (query: string) => {
+  try {
+    const data = await axios.get<ISearchResponse>(
+      `https://financialmodelingprep.com/api/v3/search?query=${query}&limit=10&exchange=NASDAQ&apikey=${import.meta.env.VITE_REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.log("error message: ", error.message);
+      return error.message;
+    } else {
+      console.log("unexpected error: ", error);
+      return "An expected error has occured.";
+    }
+  }
+};
