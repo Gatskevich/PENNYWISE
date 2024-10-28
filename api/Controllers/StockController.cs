d355eb15e83a9fe9305fedee1589eb58cb3f0f85
rodionhatskevich <rodionhatskevich@gmail.com>
2024-10-28 21:30:44 +0100

    add filtration

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -1,6 +1,7 @@
 using System;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -20,9 +21,9 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+          public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
-            var stocks = await _stockRepo.GetAllAsync();
+            var stocks = await _stockRepo.GetAllAsync(query);
 
             var stockDto = stocks.Select(s => s.ToStockDto());
 
