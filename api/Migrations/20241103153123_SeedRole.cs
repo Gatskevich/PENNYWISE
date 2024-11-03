9487052348a4549f472e748962df8e00f099cc29
rodionhatskevich <rodionhatskevich@gmail.com>
2024-11-03 16:36:03 +0100

    created registartion

---
+++ api/Migrations/20241103153123_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "46c46e5a-0536-4f1b-a254-3cb743ed4f14", null, "Admin", "ADMIN" },
+                    { "516cb1ea-f7ec-426d-8eab-cb4b0f3f97a6", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "46c46e5a-0536-4f1b-a254-3cb743ed4f14");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "516cb1ea-f7ec-426d-8eab-cb4b0f3f97a6");
+        }
+    }
+}
