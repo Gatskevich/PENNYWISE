f06a9debb7691ee1c7fd76806c38ccfa37a973db
rodionhatskevich <rodionhatskevich@gmail.com>
2024-11-06 22:05:28 +0100

    add registration

--- frontend/src/Pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/Pages/RegisterPage/RegisterPage.tsx
@@ -6,35 +6,36 @@ import { Link } from "react-router-dom";
 
 type Props = {};
 
-type LoginFormsInputs = {
+type RegisterFormsInputs = {
+  email: string;
   userName: string;
   password: string;
 };
 
 const validation = Yup.object().shape({
+  email: Yup.string().required("Email is required"),
   userName: Yup.string().required("Username is required"),
   password: Yup.string().required("Password is required"),
 });
 
-const LoginPage = (props: Props) => {
-  const { loginUser } = useAuth();
+const RegisterPage = (props: Props) => {
+  const { registerUser } = useAuth();
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<LoginFormsInputs>({ resolver: yupResolver(validation) });
+  } = useForm<RegisterFormsInputs>({ resolver: yupResolver(validation) });
 
-  const handleLogin = (form: LoginFormsInputs) => {
-    loginUser(form.userName, form.password);
+  const handleLogin = (form: RegisterFormsInputs) => {
+    registerUser(form.email, form.userName, form.password);
   };
-
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mb-20 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Login to your account
+                Signup
             </h1>
             <form
               className="space-y-4 md:space-y-6"
@@ -43,6 +44,26 @@ const LoginPage = (props: Props) => {
               <div>
                 <label
                   htmlFor="email"
+                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                >
+                  Email
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="Email"
+                  {...register("email")}
+                />
+                {errors.email ? (
+                  <p className="text-red-500">{errors.email.message}</p>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div>
+                <label
+                  htmlFor="username"
                   className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                 >
                   Username
@@ -92,15 +113,15 @@ const LoginPage = (props: Props) => {
                 type="submit"
                 className="w-full text-white bg-lightGreen hover:opacity-70 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
               >
-                Login
+                Signup
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Don’t have an account yet?{" "}
+                Already have an account?{" "}
                 <Link
-                  to="/register"
+                  to="/login"
                   className="text-blue-700 font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
-                  Sign up
+                  Login
                 </Link>
               </p>
             </form>
@@ -111,4 +132,4 @@ const LoginPage = (props: Props) => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
