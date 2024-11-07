da9e737104286a1f596d272d27f373f5dc1ae7a2
rodionhatskevich <rodionhatskevich@gmail.com>
2024-11-07 19:01:47 +0100

    add to get comments

--- frontend/src/Components/StockComment/StockComment.tsx
+++ frontend/src/Components/StockComment/StockComment.tsx
@@ -1,7 +1,10 @@
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 import StockCommentForm from "./StockCommentForm/StockCommentForm";
-import {  commentPostAPI } from "../../Services/CommentService";
+import {  commentGetAPI, commentPostAPI } from "../../Services/CommentService";
 import { toast } from "react-toastify";
+import { CommentGet } from "../../Models/Comment";
+import Spinner from "../Spinner/Spinner";
+import StockCommentList from "../StockCommentList/StockCommentList";
 
 type Props = {
   stockSymbol: string;
@@ -13,23 +16,40 @@ type CommentFormInputs = {
 };
 
 const StockComment = ({ stockSymbol }: Props) => {
+    const [comments, setComment] = useState<CommentGet[] | null>(null);
+    const [loading, setLoading] = useState<boolean>();
+
     const handleComment = useCallback((e: CommentFormInputs) => {
         commentPostAPI(e.title, e.content, stockSymbol)
         .then((res) => {
             if (res) {
                 toast.success("Comment created successfully!");
+                getComments();
             }
         })
         .catch((e) => {
             toast.warning(e);
         });
-  }, [stockSymbol]);
+    }, [stockSymbol]);
+
+    const getComments = useCallback(() => {
+        setLoading(true);
+        commentGetAPI(stockSymbol).then((res) => {
+          setLoading(false);
+          setComment(res?.data!);
+        });
+    }, [stockSymbol]);
+
+    useEffect(() => {
+        getComments();
+    }, []);
   
-  return (
-    <div className="flex flex-col">
-      <StockCommentForm handleComment={handleComment} />
-    </div>
-  );
+    return (
+        <div className="flex flex-col">
+        {loading ? <Spinner /> : <StockCommentList comments={comments!} />}
+        <StockCommentForm handleComment={handleComment} />
+        </div>
+    );
 };
 
 export default StockComment;
